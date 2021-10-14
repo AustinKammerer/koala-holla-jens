487aac244a81f384deb8710ae0c3e5e8554f0e94
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-10-14 14:55:36 -0500

    pool added to koala router

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+const pg = require(`pg`);
+
+const Pool =pg.Pool;
+
+const pool = new Pool({
+
+    database:`koalas`,
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+pool.on(`connect`, () => {
+    console.log('postgreSql connected');
+});
+pool.on(`error`, (error) => {
+    console.log('ERROR connecting to postgreSQL', error);
+});
 // DB CONNECTION
 
 
